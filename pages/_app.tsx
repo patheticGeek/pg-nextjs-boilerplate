98ab5740a494b3b4fcfd4760c041d338011a1af7
patheticGeek <geekpathetic@gmail.com>
2021-07-11 10:50:02 +0530

    💄 ✨ Added twind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import React from 'react'
 
-import '../styles/globals.css'
+import '@utils/twindSetup'
+import 'twind/shim'
+
+import '@styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next App Boilerplate</title>
+        <meta name="description" content="Next App Boilerplate" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
